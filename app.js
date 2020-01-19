e3aaa2361f26a9e456b97337923dd6bc326fff79
sammy640 <samratsomanna99@gmail.com>
2020-01-19 11:26:31 -0500

    Added animation

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 'use strict';
 console.log("Here\'s a hidden message");
 
-/*  var slideNum = 1;
-next(slideNum);
+function scroll() {
+    var description = document.querySelector('.description');
+    var descriptionPosition = description.getBoundingClientRect().top;
+    console.log(descriptionPosition);
+    var screenPosition = window.innerHeight / 1.5;
 
-function plus(n) {
-  next(slideNum += n);
-}
-
-function next(n) {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  if (n > x.length) {slideNum = 1}
-  if (n < 1) {slideNum = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    if(descriptionPosition < screenPosition) {
+      description.classList.add('description_appear');
+    }
   }
-  x[slideNum-1].style.display = "inline";
-}
-*/
+
+  window.addEventListener('scroll', scroll);
+
+  
+
